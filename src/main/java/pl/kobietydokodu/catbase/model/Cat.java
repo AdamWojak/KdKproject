1a4ac533aa3f37430c557a9aab5f3fb397174df7
Adam Wojak <adam.wojak@wp.eu>
2018-02-27 12:05:44 +0100

    lesson 1

--- src/main/java/pl/kobietydokodu/catbase/model/Cat.java
+++ src/main/java/pl/kobietydokodu/catbase/model/Cat.java
@@ -1,5 +1,16 @@
 package pl.kobietydokodu.catbase.model;
 
+import java.util.Date;
+
 public class Cat {
+
+	String name;
+	Date dateOfBirth;
+	Float weight;
+	String ownerName;
+
+	public String introduceYourself() {
+		return "Cat " + name + ", born " + dateOfBirth + ", weight: " + weight + ", owner name: " + ownerName;
+	}
 
 }
